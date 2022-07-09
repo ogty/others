335e52561ab1ba8a34f7905d3fc53062ce87898f
Yoshinobu Ogata <92501820+ogty@users.noreply.github.com>
2022-07-09 23:49:07 +0900

    Create story_book_creator.sh

---
+++ scripts/story_book_creator.sh
@@ -0,0 +1,82 @@
+#!/bin/sh
+
+# Description:
+#     Command to create a storybook from component names.
+#
+# Options:
+#     -a, --atoms: atoms
+#     -m, --molecules: molecules
+#     -o, --organisms: organisms
+#     -t, --templates: templates
+#     -p, --pages: pages
+#     -c, --custom <custom_name>: custom
+#
+# Usage:
+#     $ source story_book_creator.sh YourComponent -a
+#     $ source story_book_creator.sh YourComponent -c customComponentUnit
+
+template="import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Meta, Story } from '@storybook/react';
+
+import {{componentName}} from './{{componentName}}';
+
+export default {
+    title: '{{unitName}}/{{componentName}}',
+    component: {{componentName}},
+} as Meta;
+
+const Template: Story<typeof {{componentName}}> = (args) => {
+  return (
+    <Router>
+      <{{componentName}} {...args}>
+        <div />
+      </{{componentName}}>
+    </Router>
+  );
+};
+
+export const Primary: Template = Template.bind({});
+// Primary.args = {};"
+
+unitName=""
+projectType="tsx"
+componentName="$1"
+
+# processing option
+while [ $# -gt 0 ]; do
+    case "$2" in
+        -a|--atoms)
+            unitName="atoms"
+            shift
+            ;;
+        -m|--molecules)
+            unitName="molecules"
+            shift
+            ;;
+        -o|--organisms)
+            unitName="organisms"
+            shift
+            ;;
+        -t|--templates)
+            unitName="templates"
+            shift
+            ;;
+        -p|--pages)
+            unitName="pages"
+            shift
+            ;;
+        -c|--custom)
+            shift
+            unitName="$2"
+            ;;
+        *)
+            break
+            ;;
+    esac
+done
+
+echo "$template" \
+ | sed "s/{{unitName}}/$unitName/g" \
+ | sed "s/{{componentName}}/$componentName/g" \
+ > "./$componentName.stories.$projectType"
