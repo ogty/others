6dfb88efff68ac66661ba80e69c31ad00b5ec4fd
ogty <92501820+ogty@users.noreply.github.com>
2022-07-25 06:25:32 +0900

    Create main.rs

---
+++ quine/main.rs
@@ -0,0 +1,9 @@
+fn main() {
+    let x = "fn main() {\n    let x = ";
+    let y = "print!(\"{}{:?};\n    let y = {:?};\n    {}\", x, x, y, y)\n}\n";
+    print!("{}{:?};
+    let y = {:?};
+    {}", x, x, y, y)
+}
+
+// fn main(){print!("{},{0:?})}}","fn main(){print!(\"{},{0:?})}}\"")}
