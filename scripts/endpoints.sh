2c4171ed0b3bd9dab020853349553a74f0f7fd90
Yoshinobu Ogata <92501820+ogty@users.noreply.github.com>
2022-12-04 23:54:56 +0900

    Create endpoints.sh

---
+++ scripts/endpoints.sh
@@ -0,0 +1,50 @@
+# Usage
+# ▀▀▀▀▀▀
+#
+#    ╭─ Zsh ─────────────────────────────────────────────────────────────────────────────╮
+#    │  $ source endpoints.sh [output format] [code type]                                │
+#    ╰───────────────────────────────────────────────────────────────────────────────────╯
+#
+# Options
+# ▀▀▀▀▀▀▀▀
+#
+#    ○ Output format
+#        □ list(default)
+#        □ code
+#    ○ Code type
+#        □ httpie(default)
+#        □ curl
+#
+# Examples
+# ▀▀▀▀▀▀▀▀▀
+#
+#    ╭─ Zsh ─────────────────────────────────────────────────────────────────────────────╮
+#    │  $ source endpoints.sh                                                            │
+#    │  $ source endpoints.sh code                                                       │
+#    │  $ source endpoints.sh code curl                                                  │
+#    ╰───────────────────────────────────────────────────────────────────────────────────╯
+
+LIST_PATTERN='s/^(.+api\/)(.+)/- [`\2`](\1\2)/g'
+CODE_CURL_PATTERN='s/^(.+api\/)(.+)/\n**`\2`**\n\n```\ncurl \1\2\n```/g'
+CODE_HTTPIE_PATTERN='s/^(.+api\/)(.+)/\n**`\2`**\n\n```\nhttp \1\2\n```/g'
+
+files=$(find src/pages/api -type f)
+
+for file in $files; do
+    endpoints=$(echo $file \
+    | sed -r 's/(.+)\[(.+)\].ts/\1:\2/' \
+    | sed -r 's/src\/pages/http:\/\/localhost:3000/' \
+    | sed -r 's/\.ts//')
+
+    if [ "$1" = "code" ]; then
+        if [ "$2" = "curl" ]; then
+            endpoints=$(echo "$endpoints" | sed -r "$CODE_CURL_PATTERN")
+        else
+            endpoints=$(echo "$endpoints" | sed -r "$CODE_HTTPIE_PATTERN")
+        fi
+    else
+        endpoints=$(echo "$endpoints" | sed -r "$LIST_PATTERN")
+    fi
+
+    echo $endpoints
+done
