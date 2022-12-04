7b5733f87f766cd9bff4581d5f2667391e2d795d
Yoshinobu Ogata <92501820+ogty@users.noreply.github.com>
2022-12-05 00:23:52 +0900

    Update endpoints.sh

--- scripts/endpoints.sh
+++ scripts/endpoints.sh
@@ -25,6 +25,9 @@
 #    │  $ source endpoints.sh code curl                                                  │
 #    ╰───────────────────────────────────────────────────────────────────────────────────╯
 
+
+EXTENSION='ts'
+EXTENSION_PATTERN="s/\.$EXTENSION//g"
 LIST_PATTERN='s/^(.+api\/)(.+)/- [`\2`](\1\2)/g'
 CODE_CURL_PATTERN='s/^(.+api\/)(.+)/\n**`\2`**\n\n```\ncurl \1\2\n```/g'
 CODE_HTTPIE_PATTERN='s/^(.+api\/)(.+)/\n**`\2`**\n\n```\nhttp \1\2\n```/g'
@@ -35,7 +38,7 @@ for file in $files; do
     endpoints=$(echo $file \
     | sed -r 's/(.+)\[(.+)\].ts/\1:\2/' \
     | sed -r 's/src\/pages/http:\/\/localhost:3000/' \
-    | sed -r 's/\.ts//')
+    | sed -r $EXTENSION_PATTERN)
 
     if [ "$1" = "code" ]; then
         if [ "$2" = "curl" ]; then
